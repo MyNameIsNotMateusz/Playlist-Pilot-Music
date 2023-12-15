18f930f3b7c8c60d8b008b7165ba8e96682a5f7f
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-12-15 19:14:23 +0100

    I almost finished my app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MainApp from "./app/mainApp";
+
+function App() {
+  return (
+    <div className="App">
+      <MainApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/mainApp.js
@@ -0,0 +1,225 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar, Form, Button, InputGroup, Row, Col, Badge, Card, Spinner, Alert } from 'react-bootstrap';
+import './styles.css';
+import { useEffect, useState } from 'react';
+
+const CLIENT_ID = "337be670400741cc9308edd31aae2db6";
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
+const SPACE_DELIMITER = "%20";
+const SCOPES = ["user-read-email", "user-read-private"]
+const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
+const MainApp = () => {
+
+
+    const [login, setLogin] = useState(() => {
+        let link = window.location.href;
+        if (link.length > 30) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    const [inputSong, setInputSong] = useState("");
+    const [songList, setSongList] = useState([]);
+    const [playlist, setPlaylist] = useState([]);
+
+    //Obtains an access token to perform the rest of the operations.
+    const [accessToken, setAccessToken] = useState("");
+
+    const handleLogin = () => {
+        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+    }
+
+    useState(() => {
+
+        let link = window.location.href;
+
+        if (link.length > 30) {
+            const token = window.location.href.slice(36, 247);
+            setAccessToken(token);
+        }
+
+    }, []);
+
+
+    //The entire function searches for songs and displays them
+    const searchSong = async () => {
+
+        const songName = inputSong;
+
+        const authParameters = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }
+
+        try {
+            const response = await fetch('https://api.spotify.com/v1/search?q=' + songName + '&type=track', authParameters);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                setSongList(jsonResponse.tracks.items);
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        const songInput = document.querySelector(".songInput");
+        songInput.value = "";
+    }
+
+    //This function is used to add a song to a playlist
+    const addToPlaylist = (event) => {
+        const indexSong = event.target.id;
+        setPlaylist((prev) => {
+            return [...prev, songList[indexSong]]
+        });
+    }
+
+
+
+    //tutaj robie sobie jakies kurwa testy
+
+    const removeItem = (event) => {
+        const arrayPlaylist = document.querySelector(".playlistContainer").children;
+        const itemToRemove = event.target.id;
+        let index;
+
+        for (let i = 0; i < arrayPlaylist.length; i++) {
+            if (arrayPlaylist[i].id == itemToRemove) {
+                index = i - 1;
+            }
+        }
+
+        setPlaylist((prev) => {
+            return [
+                ...prev.filter((item, idx) => {
+                    return idx !== index;
+                })
+            ]
+        })
+
+    };
+
+    return (
+        <>
+            {!login
+                ?
+                <div className="loading">
+                    <Button onClick={handleLogin}>Log In To Your Spotify</Button>
+                </div>
+                :
+                <div className="main">
+                    <Navbar bg="primary" data-bs-theme="dark" className="nav">
+                        <Container>
+                            <Navbar.Brand href="#">Playlist Pilot Music</Navbar.Brand>
+                            <Nav className="me-auto">
+                                <Nav.Link href="#createButton">Create A Playlist
+                                </Nav.Link>
+                                <Nav.Link href="#home">Trending</Nav.Link>
+                                <Nav.Link href="#features">About Me</Nav.Link>
+                            </Nav>
+                        </Container>
+                    </Navbar>
+                    <Container className="search">
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter A Song Title"
+                                onChange={(event) => {
+                                    setInputSong(event.target.value)
+                                }}
+                                onKeyDown={event => {
+                                    if (event.key == "Enter") {
+                                        searchSong()
+                                    }
+                                }}
+                                className="songInput"
+                            />
+                            <Button
+                                onClick={() => {
+                                    searchSong()
+                                }}
+                                variant="primary">Search</Button>
+                        </InputGroup>
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col md={8}>
+                                <Row className="songContainer">
+                                    <h2>
+                                        Results<Badge bg="secondary">{songList.length}</Badge>
+                                    </h2>
+                                    {songList.map((item, index) => {
+                                        return (
+                                            <Card key={item["id"]} style={{ width: '18rem' }}>
+                                                <Card.Img variant="top" src={item["album"]["images"][0]["url"]} />
+                                                <Card.Body>
+                                                    <Card.Title>{item["artists"][0]["name"]}</Card.Title>
+                                                    <Card.Text>
+                                                        {item["name"]}
+                                                    </Card.Text>
+                                                    <Button id={index} onClick={addToPlaylist} variant="primary"
+                                                    >Add</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        )
+                                    })}
+                                </Row>
+                            </Col>
+                            <Col md={4}>
+                                <Row className="songContainer playlistContainer">
+                                    <h2>
+                                        New Playlist<Badge bg="secondary">{playlist.length}</Badge>
+                                    </h2>
+                                    {playlist.map((item) => {
+                                        return (
+                                            <Card id={item["id"]} key={item["id"]} style={{ width: '18rem' }}>
+                                                <Card.Img variant="top" src={item["album"]["images"][0]["url"]} />
+                                                <Card.Body>
+                                                    <Card.Title>{item["artists"][0]["name"]}</Card.Title>
+                                                    <Card.Text>
+                                                        {item["name"]}
+                                                    </Card.Text>
+                                                    <Button onClick={removeItem} id={item["id"]} variant="primary"
+                                                    >Remove</Button>
+                                                </Card.Body>
+                                            </Card>
+                                        )
+                                    })}
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container className="alertContainer">
+                        <Alert variant="danger">
+                            <Alert.Heading>Oops! You didn't enter the playlist name.</Alert.Heading>
+                            <p>
+                                Unfortunately, you haven't entered the playlist name, so we can't proceed to the next stage.
+                            </p>
+                        </Alert>
+                    </Container>
+                    <Container className="createButton" id="createButton">
+                        <div className="d-grid gap-2">
+                            <InputGroup size="lg">
+                                <Form.Control
+                                    className="inputName"
+                                    aria-label="Large"
+                                    aria-describedby="inputGroup-sizing-sm"
+                                    placeholder='Enter A Name For The playlist'
+                                />
+                            </InputGroup>
+                            <Button variant="primary" size="lg">
+                                Create A Playlist
+                            </Button>
+                        </div>
+                    </Container>
+                </div>
+            }
+        </>
+    );
+};
+
+export default MainApp;
